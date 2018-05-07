102cc1984cad9a4dfbe16e2454b8ac3092e3adac
Sam Kim <samhykim@gmail.com>
2018-05-07 00:40:11 -0700

    fix verify_address

--- contracts/HouseChain.sol
+++ contracts/HouseChain.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.21;
 
 contract HouseChain {
   mapping (address => uint256) public balances;
-  mapping (string => string) house_addresses;
+  mapping (address => string) public house_addresses;
 
   uint256 totalSupply = 21000000;
   address public owner;
@@ -13,31 +13,17 @@ contract HouseChain {
     owner = msg.sender;
   }
 
-  function add_address(string addr, string person) public returns (string name) {
-    house_addresses[addr] = person;
-    name = house_addresses[addr];
+  function add_address(address _owner, string addr) public returns (string name) {
+    house_addresses[_owner] = addr;
+    name = house_addresses[_owner];
   }
 
-  function verify_address(string addr) public view returns (string name) {
-    name = house_addresses[addr];
-  }
-
-  function transfer(address _to, uint256 _value) public returns (bool success) {
-    require(balances[msg.sender] >= _value);
-    balances[msg.sender] -= _value;
-    balances[_to] += _value;
-    emit BalanceChanged(msg.sender, balances[msg.sender]);
-    emit BalanceChanged(_to, balances[_to]);
-    return true;
+  function verify_address(address _owner) public view returns (string addr) {
+    addr = house_addresses[_owner];
+    return addr;
   }
 
   function balanceOf(address _owner) public view returns (uint256 balance) {
     return balances[_owner];
-  }
-
-  function mint(uint256 _value) public {
-    assert(msg.sender == owner);
-    balances[msg.sender] += _value;
-    totalSupply += _value;
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,82 +1,132 @@
-function app() {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import '../css/index.css';
+import Login from './login.js';
+import House from './house.js'
+
+
+class Home extends React.Component {
+  constructor (props) {
+    super (props);
+
     if (typeof web3 == 'undefined') throw 'No web3 detected. Is Metamask/Mist being used?';
     web3 = new Web3(web3.currentProvider); // MetaMask injected Ethereum provider
     console.log("Using web3 version: " + Web3.version);
+
+
+    const contractDataPromise = $.getJSON('HouseChain.json');
+    const networkIdPromise = web3.eth.net.getId(); // resolves on the current network id
+    const accountsPromise = web3.eth.getAccounts(); // resolves on an array of accounts
   
-    var contract;
-    var userAccount;
-  
-    var contractDataPromise = $.getJSON('HouseChain.json');
-    var networkIdPromise = web3.eth.net.getId(); // resolves on the current network id
-    var accountsPromise = web3.eth.getAccounts(); // resolves on an array of accounts
-  
+    this.addAddress = this.addAddress.bind (this);
+    this.verifyAddress = this.verifyAddress.bind (this);
+    this.refreshBalance = this.refreshBalance.bind (this);
+    this.setData = this.setData.bind (this);
+
+    this.data = {};
+
     Promise.all([contractDataPromise, networkIdPromise, accountsPromise])
       .then(function initApp(results) {
-        var contractData = results[0];
-        var networkId = results[1];
-        var accounts = results[2];
-        userAccount = accounts[0];
-        console.log(networkId);
-        console.log(accounts);
-        console.log(userAccount);
-  
-        // (todo) Make sure the contract is deployed on the network to which our provider is connected
+        const contractData = results[0]
+        const networkId = results[1]
+        const userAccount = results[2][0]
+        
         if (!(networkId in contractData.networks)) {
           console.log(contractData.networks);
           throw new Error("Contract not found in selected Ethereum network on MetaMask.");
         }
 
-        var contractAddress = contractData.networks[networkId].address;
-        contract = new web3.eth.Contract(contractData.abi, contractAddress);
+        const contractAddress = contractData.networks[networkId].address;
+        const contract = new web3.eth.Contract(contractData.abi, contractAddress);
+        return {
+          contractData: contractData,
+          networkId: networkId, 
+          userAccount: userAccount,
+          contract: contract
+        }
       })
-      .then(refreshBalance)
+      .then(this.setData)
+      .then(this.refreshBalance)
       .catch(console.error);
+  }
 
-    function refreshBalance() { // Returns web3's PromiEvent
-      // Calling the contract (try with/without declaring view)
-      contract.methods.balanceOf(userAccount).call().then(function (balance) {
-        $('#display').text(balance + " CDT");
-        $("#loader").hide();
-      });
-    }
-
-    function transfer(to, amount) {
-    console.log(to, amount)
-    if (!to || !amount) return console.log("Fill in both fields");
-
-    $("#loader").show();
-
-    contract.methods.transfer(to, amount).send({from: userAccount})
-      .then(refreshBalance)
-      .catch(function (e) {
-        $("#loader").hide();
-      });
-    }
-
-    function mint(amount) {
-      if (!amount) return console.log("Fill in amount");
-
-      $("#loader").show();
-
-      contract.methods.mint(amount).send({from: userAccount})
-        .then(refreshBalance)
-        .catch(function (e) {
-        console.log("cannot mint if user is not owner");
-        $("#loader").hide();
-      });
-    }
+  setData(data) {
+    this.data = data;
+  }
 
 
-    $("#button").click(function() {
-      var toAddress = $("#address").val();
-      var amount = $("#amount").val();
-      transfer(toAddress, amount);
+  refreshBalance() { // Returns web3's PromiEvent
+    // Calling the contract (try with/without declaring view)
+    console.log("The user account is "+this.data.userAccount)
+    console.log(this.data.contract)
+    this.data.contract.methods.balanceOf(this.data.userAccount).call().then(function (balance) {
+      $('#display').text(balance + " CDT");
+      $("#loader").hide();
+    });
+  }
+
+
+  addAddress (addr) {
+    console.log("Adding an address of "+addr+" for userAccount "+this.data.userAccount)
+    this.data.contract.methods.add_address(this.data.userAccount, addr).send({from: this.data.userAccount}).then(function (name) {
+      console.log("Address is "+name);
     });
 
-    $("#mint-button").click(function() {
-      var amount = $("#mint-amount").val();
-      mint(amount);
+  }
+
+  verifyAddress (owner) {
+    console.log("Reading an address for userAccount "+owner)
+    this.data.contract.methods.verify_address(owner).call().then(function (addr) {
+      console.log("Address is "+addr);
     });
+
+  }
+
+  render () {
+    return (
+      <div className='App'>
+        <header className="App-header">
+          <h1>India HouseChain</h1>
+          <h2 id="display"></h2>
+          <img id="loader" src="https://loading.io/spinners/pacman/lg.eat-bean-pie-loading-gif.gif"></img>
+
+          <label for="add-address" class="col-lg-2 control-label">Add Address</label>
+          <input ref="enterAddressTextBox" type="text"></input>     
+          <button onClick= { (e) => this.addAddress(this.refs.enterAddressTextBox.value) } >Add Address</button>
+
+          <label for="verify-address" class="col-lg-2 control-label">Verify Address</label>
+          <input ref="verifyAddressTextBox" type="text"></input>     
+          <button onClick= { (e) => this.verifyAddress(this.refs.verifyAddressTextBox.value) } >Verify Address</button>
+
+        </header>
+        <br />
+        <button>
+          <Link to="/login">Login</Link>
+        </button>
+        <button>
+          <Link to="/house">House</Link>
+        </button>
+      </div>
+    )
+  }
 }
-$(document).ready(app);
- 
+
+
+const Root = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/house" component={House} />
+    </div>
+  </Router>
+);
+
+
+ReactDOM.render(
+  <Root />,
+  document.getElementById('root')
+);
+
